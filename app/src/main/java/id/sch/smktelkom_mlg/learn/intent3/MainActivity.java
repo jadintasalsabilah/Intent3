6a6a833208544c9bc1c72cf9584a210b913fd386
jadintasalsabilah <jadintasalsabilahh@gmail.com>
2017-01-25 11:08:09 +0700

    intent browser

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
@@ -13,6 +13,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        findViewById(R.id.imageViewBrowser)
+                .setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        openWebPage("http://www.smktelkom-mlg.sch.id/");
+                    }
+
+                    private void openWebPage(String url) {
+                        Uri webpage = Uri.parse(url);
+                        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+                        if (intent.resolveActivity(getPackageManager()) != null)
+                            startActivity(intent);
+                    }
+                });
+
         findViewById(R.id.imageViewSMS)
                 .setOnClickListener(new View.OnClickListener() {
                     @Override
